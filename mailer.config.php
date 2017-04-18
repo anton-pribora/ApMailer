3b4fcc8c4bb24dc588f2e9cf1c42cbca196a4dce
Anton N Pribora <anton-pribora@gmail.com>
2017-04-18 14:41:56 +0500

    FIX Комментарий с примером конфига

--- mailer.config.php
+++ mailer.config.php
@@ -6,8 +6,8 @@ return [
 //  'afterSend'   => function($text, $message, $layer) { myMailLog($text); },
     'transports'  => [
         ['file', 'dir'  => __DIR__ .'/mails'],
-//      ['smtp', 'host' => 'smtp.yandex.ru', 'port' => '465', 'login' => '****@yandex.ru', 'password' => '******'],
-//      ['smtp', 'host' => 'smtp.gmail.com', 'port' => '465', 'login' => '****@gmail.com', 'password' => '******'],
+//      ['smtp', 'host' => 'smtp.yandex.ru', 'ssl' => true, 'port' => '465', 'login' => '****@yandex.ru', 'password' => '******'],
+//      ['smtp', 'host' => 'smtp.gmail.com', 'ssl' => true, 'port' => '465', 'login' => '****@gmail.com', 'password' => '******'],
 //      ['smtp', 'host' => '192.168.0.1', 'timeout' => 30],
     ],
 ];
